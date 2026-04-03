29a42bbfeaef51514535a0dd554384775344bde9
ella0920 <muchen202309@126.com>
2026-04-03 16:26:50 -0700

    Update merge_sold.py

--- week 1/merge_sold.py
+++ week 1/merge_sold.py
@@ -7,4 +7,9 @@ files = sorted([f for f in os.listdir(folder) if f.endswith('.csv')])
 all_dfs = [pd.read_csv(os.path.join(folder, f), encoding='latin-1') for f in files]
 final_df = pd.concat(all_dfs, ignore_index=True)
 
+# Validation for Sold
+print(f"Total Rows: {len(final_df)}")
+print(final_df[['ClosePrice', 'DaysOnMarket', 'LivingArea']].describe())
+print(final_df['PropertyType'].value_counts())
+
 final_df.to_csv('merged_sold.csv', index=False)
